3573a2412308ec9708fe7711a4b403c5fd016a9c
binhe <binhetech@163.com>
2020-08-10 22:24:46 +0800

    update fasttext train code

--- src/04-train-fasttext.py
+++ src/04-train-fasttext.py
@@ -2,19 +2,26 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import f1_score
 
+from sklearn.model_selection import train_test_split, StratifiedKFold
 import fasttext
 
-trainData = pd.read_csv("../data/train_set.csv", sep="\t", nrows=1000)
+trainData = pd.read_csv("../data/train_set.csv", sep="\t")
 print("trainData shape={}".format(trainData.shape))
 
-testAData = pd.read_csv("../data/test_a.csv", sep="\t", nrows=100)
+testAData = pd.read_csv("../data/test_a.csv", sep="\t")
 print("testAData shape={}".format(testAData.shape))
 
 
 def gen_train_test_file(data, set):
     if set == "train":
         data["label"] = data["label"].map(lambda x: f"__label__{str(x)}")
-    data.to_csv(f"../data/{set}-fasttext.txt")
+        traind, devd = train_test_split(data, random_state=42, stratify=data["label"], test_size=0.1)
+        traind.to_csv(f"../data/train-fasttext.txt", index=False, sep=" ")
+        devd.to_csv(f"../data/dev-fasttext.txt", index=False, sep=" ")
+        print("train shape={}".format(traind.shape))
+        print("dev shape={}".format(devd.shape))
+    else:
+        data.to_csv(f"../data/{set}-fasttext.txt", index=False, sep=" ")
 
 
 def evaluate(y_true, y_pred):
@@ -27,10 +34,12 @@ gen_train_test_file(testAData, "test")
 lr = 0.5
 epoch = 20
 wordNgrams = 2
-model = fasttext.train_supervised(input="../data/train-fasttext.txt", lr=lr, epoch=epoch, wordNgrams=wordNgrams)
-print("model.labels={}".format(model.labels))
+model = fasttext.train_supervised(input="../data/train-fasttext.txt", autotuneValidationFile='../data/dev-fasttext.txt')
+print("{} words".format(len(model.words)))
+print("{} labels={}".format(len(model.labels), model.labels))
 model.save_model("../models/fasttext-model.bin")
 
-y_pred = model.test("../data/test-fasttext.txt")
+X_test = pd.read_csv("../data/test-fasttext.txt")["text"]
+y_pred = [model.predict(i)[0][0][9:] for i in X_test]
 outs = pd.DataFrame({"label": y_pred})
 outs.to_csv(f"../data/test_a_predict-fasttext-{lr}-{epoch}-{wordNgrams}.csv", index=False)
