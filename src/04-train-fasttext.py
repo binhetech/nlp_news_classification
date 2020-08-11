37a8d79b35b4800c5ee5d262f86fbd7b12a36b09
binhe <binhetech@163.com>
2020-08-11 21:56:33 +0800

    autotune of fasttext

--- src/04-train-fasttext.py
+++ src/04-train-fasttext.py
@@ -5,12 +5,6 @@ from sklearn.metrics import f1_score
 from sklearn.model_selection import train_test_split, StratifiedKFold
 import fasttext
 
-trainData = pd.read_csv("../data/train_set.csv", sep="\t")
-print("trainData shape={}".format(trainData.shape))
-
-testAData = pd.read_csv("../data/test_a.csv", sep="\t")
-print("testAData shape={}".format(testAData.shape))
-
 
 def gen_train_test_file(data, set):
     if set == "train":
@@ -28,18 +22,40 @@ def evaluate(y_true, y_pred):
     return f1_score(y_true, y_pred, average='macro')
 
 
-gen_train_test_file(trainData, "train")
-gen_train_test_file(testAData, "test")
+def write_file():
+    trainData = pd.read_csv("../data/train_set.csv", sep="\t")
+    print("trainData shape={}".format(trainData.shape))
 
-lr = 0.5
-epoch = 20
-wordNgrams = 2
-model = fasttext.train_supervised(input="../data/train-fasttext.txt", autotuneValidationFile='../data/dev-fasttext.txt')
-print("{} words".format(len(model.words)))
-print("{} labels={}".format(len(model.labels), model.labels))
-model.save_model("../models/fasttext-model.bin")
+    testAData = pd.read_csv("../data/test_a.csv", sep="\t")
+    print("testAData shape={}".format(testAData.shape))
 
-X_test = pd.read_csv("../data/test-fasttext.txt")["text"]
-y_pred = [model.predict(i)[0][0][9:] for i in X_test]
-outs = pd.DataFrame({"label": y_pred})
-outs.to_csv(f"../data/test_a_predict-fasttext-{lr}-{epoch}-{wordNgrams}.csv", index=False)
+    gen_train_test_file(trainData, "train")
+    gen_train_test_file(testAData, "test")
+
+
+def train():
+    lr = 0.5
+    epoch = 20
+    wordNgrams = 2
+    model = fasttext.train_supervised(input="../data/train-fasttext.txt",
+                                      autotuneValidationFile='../data/dev-fasttext.txt',
+                                      autotuneDuration=1200)
+    print("{} words".format(len(model.words)))
+    print("{} labels={}".format(len(model.labels), model.labels))
+    model.save_model("../models/fasttext-model.bin")
+
+    X_test = pd.read_csv("../data/test-fasttext.txt")["text"]
+    y_pred = [model.predict(i)[0][0][9:] for i in X_test]
+    outs = pd.DataFrame({"label": y_pred})
+    outs.to_csv(f"../data/test_a_predict-fasttext-{lr}-{epoch}-{wordNgrams}.csv", index=False)
+
+
+def test():
+    model = fasttext.load_model("../models/fasttext-model.bin")
+    out = model.test("../data/dev-fasttext.txt")
+    print(out)
+
+
+write_file()
+train()
+test()
